dcd1fb41e62e494d29b73f31028a0103f099e4bc
eduarmreyes <eduar.mreyes@gmail.com>
2017-06-02 20:01:47 -0600

    Integrating Angular with Serverless REST API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Http, RequestOptions } from "@angular/http";
+import { AuthHttp, AuthConfig } from "angular2-jwt";
+import { MaterialModule } from "@angular/material";
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 import { AuthService } from "./auth.service";
-import { MaterialModule } from "@angular/material";
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskFormComponent } from './task-list/task-form/task-form.component';
+
+import { TaskListService } from "./task-list/task-list.service";
+
+// creates a factory to AuthHttp
+export function authHttpFactory(http: Http, options: RequestOptions) {
+  return new AuthHttp(new AuthConfig(), http, options);
+}
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    TaskListComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule,
     MaterialModule,
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    TaskListService, // adds new service
+    {
+      provide: AuthHttp,
+      useFactory: authHttpFactory, // defines how to provide AuthHttp
+      deps: [ Http, RequestOptions ]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
